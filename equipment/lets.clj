bb34b8af5d6ff9b705fa7a00d33179fa5437e940
keychera <kyogatama@gmail.com>
2026-03-02 22:56:17 +0900

    unpack jar, clj-kondo niceties

--- equipment/lets.clj
+++ equipment/lets.clj
@@ -1,5 +1,6 @@
 (ns lets
   (:require
+   [babashka.fs :as fs]
    [clojure.string :as str]
    [clojure.tools.build.api :as b])
   (:import
@@ -7,9 +8,28 @@
 
 (defonce deps-basis (delay (b/create-basis {:project "deps.edn"})))
 
+;; adapted from https://blog.agical.se/en/posts/how-to-create-a-really-simple-clojureclr-dependency-tool/
+(defn unpack-jar [jar-path]
+  (println "unpacking" jar-path)
+  (let [deps-subdir (str/replace (fs/file-name jar-path) #".jar$" "")
+        deps-dir    (str "/app/dependencies/" deps-subdir)]
+    (fs/create-dirs deps-dir)
+    (fs/unzip jar-path deps-dir {:replace-existing true})
+    deps-dir))
+
 (defn clr [& _]
   (println "running clojure-clr pseudo deps.edn project")
-  (let [LOAD_PATH (str/join File/pathSeparator (keys (:classpath @deps-basis)))
-        cmds      ["Clojure.Main.exe" "-m" "matsu.core"]]
+  (let [classpath (into []
+                        (map (fn [path]
+                               (if (str/ends-with? path ".jar")
+                                 (unpack-jar path)
+                                 path)))
+                        (keys (:classpath @deps-basis)))
+        LOAD_PATH (str/join File/pathSeparator classpath)]
+    (try
+      (b/process {:command-args ["clj-kondo" "--lint" LOAD_PATH
+                                 "--dependencies" "--copy-configs" "--skip-lint"]})
+      (catch Throwable err
+        (println "error when running clj-kondo! cause:" (:cause (Throwable->map err)))))
     (b/process {:env {"CLOJURE_LOAD_PATH" LOAD_PATH}
-                :command-args cmds})))
+                :command-args ["Clojure.Main.exe" "-m" "matsu.core"]})))
